7c4432d9e7417da044f5be86d042e0b09eda6702
Idris Raja <idris.raja@gmail.com>
2012-04-18 19:57:59 -0700

    unit test passing for splittin between X, Y, and candidate Xs

--- 1class_knn_reg/hw1/src/functions.R
+++ 1class_knn_reg/hw1/src/functions.R
@@ -1,15 +1,19 @@
 
-stepwise.lm <- function(data_, Xi_name, Yi_name) {
+stepwise.lm <- function(data, Xi_name, Yi_name) {
     # TODO: expand comment
-    #regress Y ~ X,
-    # Yi_name <- names(data_)[Yi_name]
-    # Xi_name <- names(data_)[Xi_name]
-
-    Xi_cand <- names(data_)[which(!(names(data_) %in% c(Xi_name, Yi_name)))]
-
+    Xi_cand <- names(data)[which(!(names(data) %in% c(Xi_name, Yi_name)))]
     for (x in Xi_cand){
-        next.lm <- lm(data_[, Yi_name] ~ data_[, Xi_name] + data_[, x])
-        print(next.lm)
+        x_form <- paste(c(Xi_name, x), collapse='+')
+        form <- as.formula(sprintf('%s~%s', Yi_name, x_form))
+        next.lm <- lm(formula = form, data = data)
     }
     return(TRUE)
+}
+
+split_x_y_cand <- function(data, Xi_name, Yi_name) {
+    # takes dataframe object, list of X names already included in model, list of
+    # Y names already included in model, and returns all other column names that
+    # are candidates to be next included column in model
+    Xi_cand <- names(data)[which(!(names(data) %in% c(Xi_name, Yi_name)))]
+    return(Xi_cand)
 }
--- 1class_knn_reg/hw1/tests/test_rename_me.R
+++ 1class_knn_reg/hw1/tests/test_rename_me.R
@@ -1,18 +1,36 @@
 source('../src/functions.R')
 
-test_that('forward stepwise regression next candidate', {
-          set.seed(123)
-          x1 <- rnorm(50)
-          x2 <- 1:50
-          x3 <- x1 * sin(x2)
-          Y <- log(2)**sin(x2) + tanh(x1)
-          data_ <- cbind(data.frame(scale(cbind(x1, x2, x3))), Y)
+# test_that('forward stepwise regression next candidate', {
+          # set.seed(123)
+          # x1 <- rnorm(50)
+          # x2 <- 1:50
+          # x3 <- x1 * sin(x2)
+          # Y <- log(2)**sin(x2) + tanh(x1)
+          # data <- cbind(data.frame(scale(cbind(x1, x2, x3))), Y)
 
-          Xi_name <- 'x1'
-          Yi_name <- 'Y'
+          # Xi_name <- 'x1'
+          # Yi_name <- 'Y'
 
-          a <- stepwise.lm(data_, Xi_name, Yi_name)
+          # a <- stepwise.lm(data, Xi_name, Yi_name)
 
-          expect_true(TRUE)
-          expect_false(FALSE)
+          # expect_false(a)
+          # expect_false(FALSE)
+# })
+
+test_that('candidate columns are being properly split out', {
+          make_col_name <- function(x) sapply(x, function(x) 
+                                              paste('X', x, sep=''))
+          df <- data.frame(matrix(rnorm(50), 5, 10), Y=1:5)
+
+          x_cand_name <- split_x_y_cand(df, make_col_name(c(1, 2, 3, 4)), 'Y')
+          expect_equivalent(x_cand_name, make_col_name(5:10))
+
+          x_cand_name <- split_x_y_cand(df, make_col_name(1:8), 'Y')
+          expect_equivalent(x_cand_name, make_col_name(9:10))
+
+          x_cand_name <- split_x_y_cand(df, make_col_name(c()), 'Y')
+          expect_equivalent(x_cand_name, make_col_name(1:10))
+
+          x_cand_name <- split_x_y_cand(df, make_col_name(1:10), 'Y')
+          expect_equivalent(x_cand_name, make_col_name(c()))
 })
