ac5c613cf7ecf00d694f5deb147b357a0cc08624
Idris Raja <idris.raja@gmail.com>
2012-04-20 23:11:14 -0700

    stopped using reserved words as variable names

--- 1class_knn_reg/hw1/src/functions.R
+++ 1class_knn_reg/hw1/src/functions.R
@@ -4,56 +4,56 @@
 max_name <- function(x) names(which.max(x))
 min_name <- function(x) names(which.min(x))
 
-split_x_y_cand <- function(data, Yi_name, Xi_name) {
-    # takes dataframe object, list of X names already included in model, list of
+split_x_y_cand <- function(data_, Yi_name, Xi_name) {
+    # takes data_frame object, list of X names already included in model, list of
     # Y names already included in model, and returns all other column names that
     # are candidates to be next included column in model
-    Xi_cand <- names(data)[which(!(names(data) %in% c(Xi_name, Yi_name)))]
+    Xi_cand <- names(data_)[which(!(names(data_) %in% c(Xi_name, Yi_name)))]
 }
 
-standard_error_lm <- function(data, lm_model) {
-    pred <- predict.lm(lm_model, data, se = TRUE)
+standard_error_lm <- function(data_, lm_model) {
+    pred <- predict.lm(lm_model, data_, se = TRUE)
     stopifnot(length(pred$fit) == lm_model$model[, 1])
     residual <- pred$fit - lm_model$model[, 1]
     # see http://en.wikipedia.org/wiki/Ordinary_least_squares#Estimation
     se <- mean(residual^2)
 }
 
-next_best_regressor <- function(data, Yi_name, Xi_name) {
-    x_cand_name <- split_x_y_cand(data, Yi_name, Xi_name)
+next_best_regressor <- function(data_, Yi_name, Xi_name) {
+    x_cand_name <- split_x_y_cand(data_, Yi_name, Xi_name)
     se <- list()
     n_cv <- 10
     for(x_name in x_cand_name){
         x_form <- paste(c(Xi_name, x_name), collapse='+')
         form <- as.formula(sprintf('%s~%s', Yi_name, x_form))
-        se[x_name] <- cross_val_lm(data, form, n_cv)
+        se[x_name] <- cross_val_lm(data_, form, n_cv)
     }
     se <- unlist(se)
     return(se[which.min(se)])
 }
 
-forward_stepwise_lm <- function(data, Yi_name) {
+forward_stepwise_lm <- function(data_, Yi_name) {
     # think of a unit test for this
     Xi_name <- NULL
-    Xi_cand_name <- split_x_y_cand(data, Yi_name, Xi_name)
+    Xi_cand_name <- split_x_y_cand(data_, Yi_name, Xi_name)
 
     best <- list()
     for(Xi_name in Xi_cand_name){
-        next_best <- next_best_regressor(data, Yi_name, Xi_name)
+        next_best <- next_best_regressor(data_, Yi_name, Xi_name)
         Xi_name <- c(Xi_name, names(next_best))
         best[Xi_name] <- next_best
     }
     return(best)
 }
 
-cross_val_lm <- function(data, formula, n_cv) {
-    # again, just running regressions. but this time on subsets of the data
+cross_val_lm <- function(data_, formula_, n_cv) {
+    # again, just running regressions. but this time on subsets of the data_
     se <- rep(0, n_cv)
-    i <- 1:dim(data)[1]
+    i <- 1:dim(data_)[1]
     for(i_cv in 1:n_cv){
         test_i <- which(i %% n_cv == (i_cv - 1))
-        cv_lm <- lm(formula, data[-test_i, ])
-        se[i_cv] <- standard_error_lm(data[test_i, ], cv_lm)
+        cv_lm <- lm(formula_, data_[-test_i, ])
+        se[i_cv] <- standard_error_lm(data_[test_i, ], cv_lm)
     }
     return(sum(se))
 }
